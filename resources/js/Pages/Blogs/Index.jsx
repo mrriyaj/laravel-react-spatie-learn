6bc7ee0ca12042c32cd6e194a3486201dad17a84
InsafNilam <insafnilam.2000@gmail.com>
2024-08-29 20:23:09 +0530

    feat: Improve Blogs Index page layout and styling

--- resources/js/Pages/Blogs/Index.jsx
+++ resources/js/Pages/Blogs/Index.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, router } from "@inertiajs/react";
+import { can } from "@/Lib/utils";
+import { Button } from "@headlessui/react";
 
 export default function Index({ auth, blogs }) {
+    const user = auth.user;
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -25,7 +29,7 @@ export default function Index({ auth, blogs }) {
             <Head title="Blogs" />
             <div class="flex flex-wrap justify-center items-start mt-10">
                 {blogs.map((blog) => (
-                    <div className="p-4 max-w-sm">
+                    <div className="p-4 max-w-md">
                         <div className="flex rounded-lg h-full bg-white dark:bg-gray-800 p-8 flex-col shadow-lg transition-colors duration-300">
                             <div className="flex items-center mb-3">
                                 <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
@@ -49,23 +53,95 @@ export default function Index({ auth, blogs }) {
                                 <p className="leading-relaxed text-base text-gray-700 dark:text-gray-300 truncate transition-colors duration-300">
                                     {blog.summary}
                                 </p>
-                                <Link
-                                    href={route("blogs.show", blog.id)}
-                                    className="mt-3 text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200 inline-flex items-center transition-colors duration-300"
-                                >
-                                    Read More
-                                    <svg
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        className="w-4 h-4 ml-2"
-                                        viewBox="0 0 24 24"
+                                <div className="mt-4 flex items-center space-x-4">
+                                    <Link
+                                        href={route("blogs.show", blog.id)}
+                                        className="text-indigo-600 dark:text-indigo-400 hover:text-indigo-800 dark:hover:text-indigo-200 inline-flex items-center transition-colors duration-300"
                                     >
-                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
-                                    </svg>
-                                </Link>
+                                        Read More
+                                        <svg
+                                            fill="none"
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            className="w-4 h-4 ml-2"
+                                            viewBox="0 0 24 24"
+                                        >
+                                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                        </svg>
+                                    </Link>
+                                    {can(auth, "edit_own_blogs") &&
+                                        user.id === blog.user_id && (
+                                            <Link
+                                                href={route(
+                                                    "blogs.edit",
+                                                    blog.id
+                                                )}
+                                                className="text-yellow-600 dark:text-yellow-400 hover:text-yellow-800 dark:hover:text-yellow-200 inline-flex items-center transition-colors duration-300"
+                                            >
+                                                Edit
+                                                <svg
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    className="w-4 h-4 ml-2"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path d="M12 20h9M5.5 20h-1M7 20l-3-3V7l4-4h10l4 4v10l-3 3H7zm5-3v-8M5 8h14"></path>
+                                                </svg>
+                                            </Link>
+                                        )}
+                                    {can(auth, "delete_own_blogs") &&
+                                        user.id === blog.user_id && (
+                                            <Button
+                                                onClick={() => {
+                                                    if (
+                                                        confirm(
+                                                            "Are you sure you want to delete this blog?"
+                                                        )
+                                                    ) {
+                                                        router.delete(
+                                                            route(
+                                                                "blogs.destroy",
+                                                                blog.id
+                                                            ),
+                                                            {
+                                                                preserveScroll: true,
+                                                                onSuccess:
+                                                                    () => {
+                                                                        console.log(
+                                                                            "deleted"
+                                                                        );
+                                                                    },
+                                                                onError: () => {
+                                                                    console.log(
+                                                                        "error"
+                                                                    );
+                                                                },
+                                                            }
+                                                        );
+                                                    }
+                                                }}
+                                                className="text-yellow-600 dark:text-yellow-400 hover:text-yellow-800 dark:hover:text-yellow-200 inline-flex items-center transition-colors duration-300"
+                                            >
+                                                Delete
+                                                <svg
+                                                    fill="none"
+                                                    stroke="currentColor"
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    className="w-4 h-4 ml-2"
+                                                    viewBox="0 0 24 24"
+                                                >
+                                                    <path d="M12 20h9M5.5 20h-1M7 20l-3-3V7l4-4h10l4 4v10l-3 3H7zm5-3v-8M5 8h14"></path>
+                                                </svg>
+                                            </Button>
+                                        )}
+                                </div>
                             </div>
                         </div>
                     </div>
